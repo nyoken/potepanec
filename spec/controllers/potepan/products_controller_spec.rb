6b11647ce55f00ca98cdc1a48203f2242722bfe5
nyoken <dk525p.ss16@gmail.com>
2019-04-14 18:08:32 +0900

    Add gems related to RSpec and tests for ProductsController#show

--- spec/controllers/potepan/products_controller_spec.rb
+++ spec/controllers/potepan/products_controller_spec.rb
@@ -1,5 +1,26 @@
-#require 'rails_helper'
+require 'rails_helper'
 
-#RSpec.describe Potepan::ProductsController, type: :controller do
+RSpec.describe Potepan::ProductsController, type: :controller do
+  describe '#show' do
+    let(:product) { create(:product) }
+    subject { get :show, params: { id: product.id } }
 
-#end
+    # 正常にレスポンスを返すか
+    it "responds successfully" do
+      subject
+      expect(response).to be_success
+    end
+
+    # show.html.erbが描画されているか
+    it "render show view" do
+      subject
+      expect(response).to render_template :show
+    end
+
+    # 正しくproductが渡されているか
+    it "have correct product" do
+      subject
+      expect(assigns(:product)).to eq(product)
+    end
+  end
+end
