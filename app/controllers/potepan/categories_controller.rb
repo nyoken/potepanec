4256a42ae4838da9a5f459091f40938ca3e6b4c2
nyoken <dk525p.ss16@gmail.com>
2019-04-29 21:33:51 +0900

    Fix Categoriea show view

--- app/controllers/potepan/categories_controller.rb
+++ app/controllers/potepan/categories_controller.rb
@@ -1,7 +1,7 @@
 class Potepan::CategoriesController < ApplicationController
   def show
     @taxon = Spree::Taxon.find(params[:id])
-    @taxonomies = Spree::Taxonomy.all
+    @taxonomies = Spree::Taxonomy.all.includes(:taxons)
     @products = @taxon.all_products
   end
 end
