7d22c581cf767b99ead634618f83cd6c531e01a5
nyoken <dk525p.ss16@gmail.com>
2019-05-01 16:52:01 +0900

    Fix file names camel_case to snake_case and Remove @products from categories controller

--- app/controllers/potepan/categories_controller.rb
+++ app/controllers/potepan/categories_controller.rb
@@ -2,6 +2,5 @@ class Potepan::CategoriesController < ApplicationController
   def show
     @taxon = Spree::Taxon.find(params[:id])
     @taxonomies = Spree::Taxonomy.includes(root: :children)
-    @products = @taxon.all_products
   end
 end
