22d7119c3471605f4e838d3d7ad6778d07ad1c3c
nyoken <dk525p.ss16@gmail.com>
2019-05-03 14:23:16 +0900

    Fix Categories show sideBar from children method to leaves method

--- app/controllers/potepan/categories_controller.rb
+++ app/controllers/potepan/categories_controller.rb
@@ -1,6 +1,6 @@
 class Potepan::CategoriesController < ApplicationController
   def show
     @taxon = Spree::Taxon.find(params[:id])
-    @taxonomies = Spree::Taxonomy.includes(root: :children)
+    @taxonomies = Spree::Taxonomy.includes(:root)
   end
 end
