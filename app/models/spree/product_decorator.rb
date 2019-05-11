f330c3016c2f315a0603ddfb36258d807227b5a1
nyoken <dk525p.ss16@gmail.com>
2019-05-11 18:12:10 +0900

    Add new_products partial to root page

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -2,4 +2,6 @@ Spree::Product.class_eval do
   scope :related_products, -> (product) do
     in_taxons(product.taxons).includes(master: [:default_price, :images]).where.not(id: product.id).distinct
   end
+
+  scope :new_products, -> { includes(master: [:default_price, :images]).order(available_on: :desc) }
 end
