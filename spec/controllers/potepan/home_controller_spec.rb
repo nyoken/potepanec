25ccd8ed7c75f1ed60de93dfb92eccf70851ed5f
nyoken <dk525p.ss16@gmail.com>
2019-05-12 18:17:43 +0900

    Add home_index specs

--- spec/controllers/potepan/home_controller_spec.rb
+++ spec/controllers/potepan/home_controller_spec.rb
@@ -1,12 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Potepan::HomeController, type: :controller do
+  describe "#index" do
+    let!(:new_products) { create_list(:product, 9) }
 
-  describe "GET #index" do
-    it "returns http success" do
-      get :index
-      expect(response).to have_http_status(:success)
+    before { get :index }
+
+    # 正常にレスポンスを返すか
+    it "responds successfully" do
+      expect(response).to be_successful
+    end
+
+    # index.html.erbが描画されているか
+    it "render index view" do
+      expect(response).to render_template :index
+    end
+
+    # 正しく@new_productsが渡されているか
+    it "have correct @related_products" do
+      # 9つ以上表示されていないか（limit(NEW_PRODUCTS_LIMITが適用されているか）
+      expect(assigns(:new_products)).to match_array new_products[1..8]
     end
   end
-
 end
---
+++ spec/features/home_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.feature "Home", type: :feature do
+  let!(:products) { create_list(:product, 8) }
+
+  scenario "User accesses show page" do
+    visit potepan_path
+    within ".featuredProducts" do
+      expect(page).to have_link products.first.name, href: potepan_product_path(products.first.id)
+      expect(page).to have_content products.first.display_price
+    end
+  end
+end
